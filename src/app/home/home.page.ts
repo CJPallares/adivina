4e5a36a4bc486f84d87708014c285e2a85264916
CJPallares <cj.j.pallares@gmail.com>
2020-10-05 10:17:24 +0200

    segunda version

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  num: number;
+  mayorMenor: String = "...";
+  numSecret: number = this.numAleatorio(0,100);
 
   constructor() {}
+  numAleatorio(a,b)
+  {
+      return Math.round(Math.random() * (b - a) + parseInt(a, 10));
+  }
 
+  compruebaNumero(){
+    if(this.num)
+    {
+      if(this.numSecret < this.num)
+      {
+        this.mayorMenor = 'menor que';
+      }
+      else if(this.numSecret > this.num)
+      {
+        this.mayorMenor = 'mayor que';
+      }
+      else{
+        this.mayorMenor = '';
+      }
+    }
+  }
 }
